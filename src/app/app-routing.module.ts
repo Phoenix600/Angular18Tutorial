d46c5f042ac72f45b073a331e9c027b0ea431c64
phoenix600 <pranayramteke613@gmail.com>
2024-07-22 02:37:47 -0700

    Getting Started With Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutusComponent},
+  {path:"beans",loadChildren:()=>import ('./beans/beans.module').then((child)=>child.BeansModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
